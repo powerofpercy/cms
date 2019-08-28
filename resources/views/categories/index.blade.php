92c10f97ab341d483e542a853df5a4bd8c65c863
Percy Hu <powerofpercy@outlook.com>
2019-08-28 22:18:59 +1200

    Added ability to create and store posts

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,6 +4,7 @@
     <div class="d-flex justify-content-end mb-2">
         <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
     </div>
+
     <div class="card card-default">
         <div class="card-header">Categories</div>
         <div class="card-body">
